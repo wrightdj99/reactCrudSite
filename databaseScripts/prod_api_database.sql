bede5afb0534b8b9a7b312b5648bbf6abce9399c
wrightdj99 <wrightdj99@gmail.com>
2023-01-11 18:14:58 -0600

    Fixed SQL name error

--- databaseScripts/prod_api_database.sql
+++ databaseScripts/prod_api_database.sql
@@ -1,7 +1,9 @@
 use mydb;
-create table product(id int, name varchar(50), price int);
+create table product(id int, name varchar(20), price int);
 select * from product;
 /*To make it currency-formatted*/
 alter table product
 modify price decimal(13,2);
+alter table product
+modify name varchar(50);
 
